102977368cfef1376f8ed8b1a93578948ab7099f
Oriventi <adrianus3010@gmail.com>
2021-08-20 18:05:00 +0200

    added animation class

--- Simulator3D/src/animations/Animation.java
+++ Simulator3D/src/animations/Animation.java
@@ -1,5 +1,46 @@
 package animations;
 
-public class Animation {
+import renderEngine.DisplayManager;
 
+public abstract class Animation {
+	
+	protected float runningTime;
+	protected float currentTime;
+	protected boolean isLooped;
+	protected boolean isPaused;
+	
+	public Animation(float runningTime, boolean isLooped) {
+		this.runningTime = runningTime;
+		currentTime = 0;
+		this.isLooped = isLooped;
+		isPaused = true;
+	}
+	
+	public void update() {
+		if(!isPaused && currentTime < runningTime) {
+			currentTime += DisplayManager.getFrameTimeSeconds();
+		}else if(isLooped && !isPaused) {
+			currentTime = 0;
+		}
+	}
+	
+	public abstract float getCurrentValue();
+	
+	
+	public void startAnimation() {
+		isPaused = false;
+	}
+	
+	public void stopAnimation() {
+		isPaused = true;
+		currentTime = 0;
+	}
+	
+	public void pauseAnimation() {
+		isPaused = true;
+	}
+	
+	public void resumeAnimation() {
+		isPaused = false;
+	}
 }
