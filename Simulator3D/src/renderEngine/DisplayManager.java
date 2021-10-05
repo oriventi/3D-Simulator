174ccfe90d16c3cc4778ca2036e5d2d4d91af62c
adria <adria@DESKTOP-PDA590N.lan>
2021-10-05 18:11:37 +0200

    changed resolution

--- Simulator3D/src/renderEngine/DisplayManager.java
+++ Simulator3D/src/renderEngine/DisplayManager.java
@@ -13,8 +13,8 @@ import org.lwjgl.opengl.PixelFormat;
 
 public class DisplayManager {
 
-	public static final int WIDTH = 2560;
-	public static final int HEIGHT = 1440;
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
 	
 	public static float resizeRatio;
 	
